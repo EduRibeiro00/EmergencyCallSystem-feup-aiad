7045ca7e64c3d60e35dc5d72c0971c47f9f24f88
tiago-falves <tiago.falves98@gmail.com>
2020-11-12 12:03:47 +0000

    Minor changes

--- proj1/src/messages/VehicleResponse.java
+++ proj1/src/messages/VehicleResponse.java
@@ -2,10 +2,11 @@ package messages;
 
 public class VehicleResponse implements java.io.Serializable {
     private double value;
+    private final int NUMBER_EMPLOYEES_FACTOR = 3;
 
 
     public VehicleResponse(double distanceValue,int numberVehicles) {
-        this.value = distanceValue + numberVehicles; //TODO determinar coeficiente?
+        this.value = distanceValue + NUMBER_EMPLOYEES_FACTOR *  numberVehicles; //TODO determinar coeficiente?
         System.out.println("Distance value " + String.valueOf(distanceValue) + " Number vehicles " + numberVehicles);
     }
 
